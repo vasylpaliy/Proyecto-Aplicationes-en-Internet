1b67a1ed649e9947b052e31eb8324cd23b89b8c3
vasyl-portatil <vasilpaliy96@gmail.com>
2021-11-23 13:37:07 +0100

    añadidas funciones de buscar y filtrar

--- php/db_conection_test.php
+++ php/db_conection_test.php
@@ -2,10 +2,10 @@
 include 'db_conection.php';
 
 
-$var_consulta= "select * from movie";
-$var_resultado = $obj_conexion->query($var_consulta);
+$var_consulta_movie= "select * from movie";
+$var_resultado_movie = $obj_conexion->query($var_consulta_movie);
 
-if($var_resultado->num_rows>0)
+if($var_resultado_movie->num_rows>0)
   {
     echo"<table border='1' align='center'>
      <tr bgcolor='#E6E6E6'>
@@ -13,7 +13,7 @@ if($var_resultado->num_rows>0)
         <th>Campo2</th>
     </tr>";
 
-while ($var_fila=$var_resultado->fetch_array())
+while ($var_fila=$var_resultado_movie->fetch_array())
 {
     echo "<tr>
     <td>".$var_fila["id"]."</td>";
--- php/main.php
+++ php/main.php
@@ -1,30 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="../Estilos/style.css">
+    <link rel="stylesheet" href="../Estilos/style.css?v=<?php echo time(); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include 'db_conection.php'; ?>
+
+    <script src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js"></script>
+    <script src="../js/buscar.js"></script>
     <title>Trabajo AI</title>
 
 </head>
+
 <body>
-    
-   <div class="header">
-       <img  class="logo" src="../imagenes/film.svg" alt="Logo">
-       <div class="loggin-box">
+
+    <div class="header">
+        
+        <img class="logo" src="../imagenes/film.svg" alt="Logo">
+        
+        <div class="buscar"><input type="text" id="myInput" onkeyup="myFunction()" placeholder="Buscar..."></div>
+        
+        
+        <div class="loggin-box">
             <input type="text" placeholder="ID" name="login">
             <input type="text" placeholder="password" name="password">
-           <button>Entrar</button>
+            <button>Entrar</button>
             <p><a href="registro.php">Registrarse</a></p>
-        </div> 
-   </div>
+        </div>
+    </div>
 
-<div class="contenedor-principal">
+    <div class="contenedor-principal">
+    <table class="sortable" id="mi-tabla">
+        <tr >
+            <th width=25%>Poster</th>
+            <th width=10%>Titulo</th>
+            <th width=10%>Fecha estreno</th>
+            <th width=40%>Descripcion</th>
+            <th width=15% >Puntuacion</th>
+        </tr>
+       
+<?php
+$records = mysqli_query($obj_conexion,"SELECT * FROM movie"); // fetch data from database
+
+
+while($data = mysqli_fetch_array($records))
+{
+?>
+  <?php echo "<tr bgcolor='#E6E6E6'>";
     
+    echo "<td>"."imagen-pelicula" ."</td>";
+    echo "<td >".$data['title']."</td>" ;
+    echo "<td>".$data['date']."</td>";
+    echo "<td>".$data['desc']."</td>";
+    echo "<td><p>Nota pelicula</p></td> </tr>";
+    
+  
+   
+  
+
+}
+?>
+
+</table>
+
+
+
+<?php mysqli_close($obj_conexion); // Close connection ?>
+
+
 </div>
-<div class="footer"></div>
+
+
+    <div class="footer"></div>
 
 </body>
+
 </html>
